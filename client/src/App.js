c7b25d2b44bfbe84848eb07899b7d42a1b50a52b
nealichinohe <neal.kuni@yahoo.com>
2022-03-25 17:31:30 -0600

    successfully set up google auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
-
+import SignIn from './components/SignIn';
+import Chat from './components/Chat';
 import './App.css';
 
 function App() {
   return (
-    <>
-    </>
+    <div>
+    <SignIn/>
+    <Chat/>
+  
+    </div>
   );
 }
 
--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,16 +1,31 @@
-import firebase from "firebase";
 
-const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyBn1wfpjjE5U57z5obOQjlkK48SX-fVwSQ",
+import { initializeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBn1wfpjjE5U57z5obOQjlkK48SX-fVwSQ",
   authDomain: "chatapp-b71d2.firebaseapp.com",
   projectId: "chatapp-b71d2",
   storageBucket: "chatapp-b71d2.appspot.com",
   messagingSenderId: "751088915618",
   appId: "1:751088915618:web:023c3d00bfb701878af3fb"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const auth = getAuth(firebaseApp);
+const db = getFirestore(firebaseApp);
+
+onAuthStateChanged(auth, user => {
+  if (user != null){
+    console.log('logged in!')
+  } else {
+    console.log('No user');
+  }
 })
 
-const db = firebaseApp.firestore()
 
-const auth = firebase.auth()
-
-export {db, auth}
+export {db, auth, firebaseApp};
