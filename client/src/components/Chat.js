9eb9018ab0a490212318b9e67daad99426e0f32f
nealichinohe <neal.kuni@yahoo.com>
2022-03-30 23:43:16 -0600

    added firestore to the chat module

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { collection, query, orderBy } from "firebase/firestore"; 
 import SignOut from "./SignOut";
+import { db } from '../firebase';
 
 function Chat() {
+  const [ messages, setMessages ] =  useState([])
+
+  useEffect(()=> {
+    db.collection('messages').orderBy()
+  }, [])
+
   return (
     <div>
       <SignOut/>
